d539dfa0074a0e126a1c21559d96491e07605328
Deivy Batista <deivy.batista.ba@gmail.com>
2026-06-25 17:43:41 -0300

    function findById and delete route

--- src/app.js
+++ src/app.js
@@ -8,14 +8,22 @@ app.use(express.json()) //Indicar para express ler body com json
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo: 'G'},
     {id:2, selecao: 'Suiça', grupo: 'G'},
-    {id:3, selecao: 'Servia', grupo: 'G'},
-    {id:4, selecao: 'Camarões', grupo: 'G'}
+    {id:3, selecao: 'Camarões', grupo: 'G'},
+    {id:4, selecao: 'Servia', grupo: 'G'}
+    
 ]
+
 
 //Funçoes
 
+//Retornar o objeto por ID
 function buscarSelecaoPorId(id){
     return selecoes.filter(selecao => selecao.id == id)
+}
+
+//pegar a posição ou index do elemento no array por ID
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id ==id)
 }
 
 
@@ -35,6 +43,13 @@ app.get("/selecoes/:id", (req, res) => {
 app.post("/selecoes", (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso.')
+})
+
+app.delete("/selecoes/:id", (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)// passa posição e quantos elementos cortar
+    //splice método usado para remover elemento de um array
+    res.send(`Seleção com id ${req.params.id} excluida com sucesso!`)
 })
 
 
