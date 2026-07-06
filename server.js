537d48e4d7c167eb28307ab653c422a3a8465a6e
Deivy Batista <deivy.batista.ba@gmail.com>
2026-07-06 13:32:28 -0300

    persistencia_dados_mysql

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import app from './src/app.js'
+
+import conexao from './infra/conexao.js'
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log("Rodando na porta http://localhost:3000")
+//Fazer a conexão
+conexao.connect((erro) => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('Conexão realizada com sucesso!')
+        
+        //Escutar a porta
+        app.listen(PORT, () => {
+            console.log("Rodando na porta http://localhost:3000")
+        })
+    }
 })
