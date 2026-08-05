bc4f77b258914fb0dd606beaa571c951cd6903da
Deivy Batista <deivy.batista.ba@gmail.com>
2026-08-05 10:23:49 -0300

    Iniciando projeto frontend utilizando Handlebars e configurando app para entender handlebars

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express, { json } from "express"
+import { engine } from "express-handlebars" //Faz com que o Express entenda o Handlebars
+import path from "node:path" //Ajuda a construir caminhos de pastas corretamente
+import conexao from './app/database/conexao.js'
+import routes from './routes.js'
+
+
+const app = express()
+
+//Registra Handlebars
+app.engine(
+    "handlebars",
+    engine({
+        defaultLayout: false
+    })
+)
+
+//Definindo o mecanismo
+//Informa que o mecanismo padrão das páginas sera o Handlebars
+app.set("view engine", "handlebars")
+
+//Informar onde as pastas estão
+app.set(
+    "views",
+    path.resolve("frontend/views")
+)
+
+app.use(express.json()) //Indicar para express ler body com json
+
+//usar o routes
+app.use(routes)
+
+export default app
